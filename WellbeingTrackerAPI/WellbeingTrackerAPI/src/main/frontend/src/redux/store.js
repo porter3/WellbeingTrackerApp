32d41a79beb06669ddb58432050b4a60b324f8a8
porter3 <jakeporter310@gmail.com>
2020-06-18 13:12:31 -0400

    added thunk and logger middleware

--- WellbeingTrackerAPI/WellbeingTrackerAPI/src/main/frontend/src/redux/store.js
+++ WellbeingTrackerAPI/WellbeingTrackerAPI/src/main/frontend/src/redux/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-// thunk
-// logger
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)))
 
 export default store
